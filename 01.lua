a2f3ea5c433bc10fd63f9178fb06ba4949037cf1
Isaac Freund <mail@isaacfreund.com>
2021-12-03 02:05:38 +0100

    day 01: improve style to match day 02
    It's crazy how fast one gets better at a language in the first few days.

--- 01.lua
+++ 01.lua
@@ -1,30 +1,31 @@
-increases = 0
-previous = nil
-for line in io.lines("input/01.txt") do
-    if not previous then previous = tonumber(line) end
-    if tonumber(line) > previous then
-        increases = increases + 1
-    end
-    previous = tonumber(line)
+local f = assert(io.open("input/01.txt"))
+local input = f:read("*all")
+f:close()
+
+local lines = string.gmatch(input, "[^\n]+")
+local previous = tonumber(lines())
+local increases = 0
+for line in lines do
+	if tonumber(line) > previous then
+		increases = increases + 1
+	end
+	previous = tonumber(line)
 end
 
 print("Part 1: ", increases)
 
-increases = 0
-window = {0, 0, 0}
-i = 0
-for line in io.lines("input/01.txt") do
-    if i < 3 then
-        window[i % 3] = tonumber(line)
-    else
-        previous = window[0] + window[1] + window[2]
-        window[i % 3] = tonumber(line)
-        current = window[0] + window[1] + window[2]
-        if current > previous then
-            increases = increases + 1
-        end
+local lines = string.gmatch(input, "[^\n]+")
+local window = { tonumber(lines()), tonumber(lines()), tonumber(lines()) }
+local increases = 0
+local i = 0
+for line in lines do
+	local previous = window[1] + window[2] + window[3]
+	window[i % 3 + 1] = tonumber(line)
+	local current = window[1] + window[2] + window[3]
+	if current > previous then
+		increases = increases + 1
 	end
-    i = i + 1
+	i = i + 1
 end
 
 print("Part 2: ", increases)
